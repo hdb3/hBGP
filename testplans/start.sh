fcb5d99cab3eaa939d4533199fa40822f5a422aa
Nicholas Hart <nic@hdb3.net>
2020-02-17 09:58:39 +0000

    start on dicumentation of test scripts

---
+++ testplans/start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -
+
+if [[ -d $1 ]] ; then wd="$1"; else wd="/home/nic/src/hBGP.master/testplans" ; fi
+if [[ -f $2 ]] ; then script="$2"; else script="script1.sh" ; fi
+if [[ -f "$wd/$script" ]] ; then echo "script OK"; else echo "$wd/$script is not a regular file"; exit 1 ; fi
+
+echo "wd is $wd, script is $script"
+if tmux kill-session -t test ; then echo "killed old session" ; else echo "no session to kill" ; fi
+
+# gnome-terminal --full-screen -- tmux new-session -s test -c $wd /bin/bash "$wd/$script"
+tmux set-window-option -g remain-on-exit on
+tmux new-session -d -s test -c $wd /bin/bash "$wd/$script"
+tmux new-window -t test:1 -c $wd /bin/bash "$wd/$script"
+tmux new-window -t test:1 -c $wd /bin/bash "$wd/$script"
