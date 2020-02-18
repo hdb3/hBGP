450c17fe5a52531e6e21e5c8ed7efcf9517b5cf4
nic hart <nic@hdb3.net>
2020-02-18 17:54:15 +0000

    added automation for cisco bootstrap process

---
+++ testplans/bootstrap.ex
@@ -0,0 +1,75 @@
+#!/usr/bin/expect
+set vm [lindex $argv 0]
+set config [lindex $argv 1]
+set f [open $config]
+set data [read $f]
+log_user 0
+#log_file -noappend bootstrap.log
+
+proc ts {} {
+  set s [timestamp -format %X]
+  send_user "$s "
+}
+
+
+spawn /usr/bin/virsh start $vm
+expect "Domain $vm started"
+
+ts
+
+send_user "domain started\n"
+
+spawn /usr/bin/virsh console $vm
+
+expect { "Escape character is ^]" { ts ; send_user "console connected\n" } 
+         -re ".*\n" { send_user . }
+         timeout { send_user ~ ; exp_continue }
+       }
+
+ts
+
+send_user "waiting for config dialog\n"
+
+expect {
+        -re "Would you like to enter the initial configuration dialog" { ts ; send_user "phase 1 complete\n"; send "no\r\n" }
+        -re ".*\n" { send_user "." ; exp_continue }
+         timeout { send_user ~ ; exp_continue }
+       }
+
+ts
+
+send_user "\nmove to phase 2\n"
+
+expect {
+         -re "Press RETURN to get started" {ts ; send "\r\n"; exp_continue }
+         -re "Router>" {ts ; send_user "initialisation complete\n"}
+         -re "\n" { send_user . ; exp_continue }
+         timeout { ts ; send_user ^ ; send "\r\n" ; exp_continue }
+       }
+
+ts
+
+send_user "login complete, sending config $config\n"
+log_user 1
+send "\renable\r\rconfig terminal\r"
+
+send "$data"
+send "\r\rwrite terminal\r\r"
+
+ts
+
+send_user "config complete\n"
+
+# interact 
+
+sleep 1
+
+send "\rreload\r"
+
+expect { "[confirm]" { send "\r" } }
+
+sleep 2
+
+send "\035"
+
+# spawn /usr/bin/virsh destroy $vm 
--- testplans/buildvios.sh
+++ testplans/buildvios.sh
@@ -1,9 +1,14 @@
-qemu-img convert -f vmdk -O qcow2 /home/nic/virl/vios-adventerprisek9-m.vmdk.SPA.157-3.M3 vios1.qcow2
+vmdk=/home/nic/VIRL/vios-adventerprisek9-m.vmdk.SPA.157-3.M3
+virsh destroy vios1 || echo "nowt to do"
+virsh undefine vios1 || echo "nowt to do again"
+sudo rm -f vios1.qcow2
+qemu-img convert -f vmdk -O qcow2 $vmdk vios1.qcow2
 virt-install --os-variant freebsd12.0 --noautoconsole --import --disk vios1.qcow2 --name vios1 --memory 2048 --network network=default1,model=e1000 --network network=default2,model=e1000 --network network=default2,model=e1000 --network network=default2,model=e1000
-virsh console vios1
+#virsh console vios1
+./bootstrap.ex vios1 br1.startup.cfg
 exit 0
 for vm in vios3a vios3b vios3c
-  do qemu-img convert -f vmdk -O qcow2 /home/nic/virl/vios-adventerprisek9-m.vmdk.SPA.157-3.M3 $vm.qcow2
+  do qemu-img convert -f vmdk -O qcow2 $vios.vmdk $vm.qcow2
      virt-install --os-variant freebsd12.0 --noautoconsole --import --disk $vm.qcow2 --name $vm --memory 2048 --network network=default1,model=e1000 --network network=default2,model=e1000
      virsh console $vm
      virsh destroy $vm
