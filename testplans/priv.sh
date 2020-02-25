c3b3744437a5708a15879e3174a2fe75f3b63a12
Nicholas Hart <nic@hdb3.net>
2020-02-25 10:23:56 +0000

    moving test scripts out of code database

--- testplans/priv.sh
+++
@@ -1,8 +0,0 @@
-sudo groupadd pcap
-sudo usermod -a -G pcap $USER
-for app in tcpdump dumpcap ; do
-  sudo chgrp pcap `which $app`
-  sudo setcap cap_net_raw,cap_net_admin=eip `which $app`
-done
-
-sudo sysctl net.ipv4.ip_unprivileged_port_start=179
--- testplans/restart1.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-for vm in br1 br3a br3b br3c ; do virsh destroy $vm ; done
-virsh start br1
