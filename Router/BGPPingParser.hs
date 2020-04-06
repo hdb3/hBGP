bb4452a77c7af19a60830bea0d11f27692470b15
Nicholas Hart <nic@hdb3.net>
2020-04-06 14:53:24 +0100

    separate generic functions from BGP specific in Router/BGPPingParser.hs and Router/ParserTest.hs

--- Router/BGPPingParser.hs
+++ Router/BGPPingParser.hs
@@ -1,108 +1,70 @@
 {-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
 
-module Router.BGPPingParser where
+module Router.BGPPingParser
+  ( getGenerator,
+    getNext,
+  )
+where
 
-import BGPlib.AttoBGP (bgpParser, bgpParser1)
-import BGPlib.BGPlib (toHex, BGPMessage(..))
--- import Control.Concurrent
--- import Control.Monad (forever)
--- import Control.Monad.Extra (ifM)
-import Data.Attoparsec.ByteString (IResult (..), Parser, feed, maybeResult, parse, parseWith)
+import Data.Attoparsec.ByteString (IResult (..), Parser, feed, parse)
 import qualified Data.ByteString as ByteString
--- import Data.ByteString.Builder
--- import Data.ByteString.Builder.Extra (byteStringCopy)
--- import Data.IORef
--- import Data.IP
--- import Data.List (partition)
--- import Data.Maybe (fromJust)
--- import Foreign.C.Error
--- import GHC.IO.Exception (ioe_description)
--- import qualified Network.Socket as NS
--- import System.Environment (getArgs)
--- import System.Exit (die, exitSuccess)
-import System.IO
-
--- import System.IO.Error
--- import Text.Read (readMaybe)
-
-main = example
-
-myGetNext = getNext (ByteString.hGetSome stdin 65536) parser
+import Data.IORef
 
 type BS = ByteString.ByteString
 
--- type Val = BGPMessage
--- action = display
--- parser = bgpParser1
-
-type Val = [BGPMessage]
-action = mapM_ display
-parser = bgpParser
-
-display m = putChar $ case m of (BGPUpdate {..}) -> if | ByteString.null attributes ->  'E'  
-                                                       | null nlri -> 'A' 
-                                                       | null withdrawn -> 'u'                                           
-                                                       | otherwise -> 'U'
-                                (BGPNotify {}) -> 'N'
-                                BGPKeepalive -> 'K'
-                                BGPTimeout -> 'T'
-                                BGPEndOfStream -> 'X'
-                                (BGPError _ ) -> '?'
-
-getNext :: IO BS -> Parser Val -> BS -> IO (Maybe (BS, Val))
+getNext :: IO BS -> Parser a -> BS -> IO (Maybe (BS, a))
 getNext getter myParser bs = go (parse myParser bs)
   where
     go result = case result of
       (Done i r) -> return $ Just (i, r)
-      f@(Fail _ _ _) -> failHandler f
-      f@(Partial _) -> do
+      f@Partial {} -> do
         bs <- getter
         if ByteString.null bs then return Nothing else go (feed f bs)
+      (Fail _ context description) -> do
+        putStrLn $ "parse failed: " ++ description
+        mapM_ putStrLn context
+        return Nothing
 
 -- An example of this strategy:
 
-getterNoisy = do
-  putStrLn "enter getter for 64K"
-  bs <- ByteString.hGetSome stdin _64K
-  putStrLn $ "exit getter with " ++ show (ByteString.length bs) ++ " bytes"
-  return bs
-
-_64K = 65536 :: Int
-_1M = 1024 * 1024 :: Int
-_16M = 16 * _1M
-getterQuiet = ByteString.hGetSome stdin _16M
-
-getter = getterQuiet
-
-example =
-  let go bs = do
-        next <- getNext getter parser bs
-        maybe
-          (return ())
-          (\(bs', val) -> action val >> go bs')
-          next
-   in go ByteString.empty
-
-{-
+-- example1 =
+--   let go bs = do
+--         next <- getNext getter parser bs
+--         maybe
+--           (return ())
+--           (\(bs', val) -> action val >> go bs')
+--           next
+--    in go ByteString.empty
 
 -- This completes a circle of reasoning - the next step here is to recognize the immutable pattern erected around the ‘action val’ expression and build a control flow abstraction, which returns to the first proposed approach!  Nonetheless, a complete instance providing getNext as a simple IO action has value, and requires an IORef bound to an IO function to produce a handle:
 
+getNextIORef :: IO BS -> Parser a -> IORef BS -> IO (Maybe a)
 getNextIORef getter parser ioref = do
   bs <- readIORef ioref
-  let (bs’,val) = getNext getter parser bs
-  writeIORef ioref bs’
-  return val
+  next <- getNext getter parser bs
+  maybe
+    (return Nothing)
+    ( \(bs', val) -> do
+        writeIORef ioref bs'
+        return $ Just val
+    )
+    next
 
 -- this can be initialised into a simple genrator as follows:
-
-getGenerator getter parser = getNextIORef getter parser <$> (newIORef empty)
-
--}
--- fail handler generic:
-
-failHandler (Fail _ context description) = do
-  putStrLn $ "parse failed: " ++ description
-  mapM_ putStrLn context
-  error "error value from parse fail"
+getGenerator :: IO BS -> Parser a -> IO (IO (Maybe a))
+getGenerator getter parser = getNextIORef getter parser <$> newIORef ByteString.empty
+-- example = do
+--   putStrLn "start of function"
+--   gen <- getGenerator getter parser
+--   let go = do
+--         next <- gen
+--         maybe
+--           (putStrLn "end of stream")
+--           ( \val -> do
+--               action val
+--               go
+--           )
+--           next
+--   go
+--   putStrLn "end of function"
--- Router/ParserTest.hs
+++ Router/ParserTest.hs
@@ -1,27 +1,75 @@
+{-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Main where
 
-import qualified Router.BGPPingParser
--- import BGPlib.AttoBGP (bgpParser, bgpParser1)
--- import BGPlib.BGPlib (BGPMessage)
--- import Control.Concurrent
--- import Control.Monad (forever)
--- import Control.Monad.Extra (ifM)
--- import Data.Attoparsec.ByteString (IResult (..), Parser, feed, maybeResult, parse, parseWith)
--- import qualified Data.ByteString as ByteString
--- import Data.ByteString.Builder
--- import Data.ByteString.Builder.Extra (byteStringCopy)
--- import Data.IORef
--- import Data.IP
--- import Data.List (partition)
--- import Data.Maybe (fromJust)
--- import Foreign.C.Error
--- import GHC.IO.Exception (ioe_description)
--- import qualified Network.Socket as NS
--- import System.Environment (getArgs)
--- import System.Exit (die, exitSuccess)
--- import System.IO
--- import System.IO.Error
--- import Text.Read (readMaybe)
+import BGPlib.AttoBGP (bgpParser, bgpParser1)
+import BGPlib.BGPlib (BGPMessage (..))
+import qualified Data.ByteString as ByteString
+import Router.BGPPingParser
+import System.IO
 
-main=Router.BGPPingParser.example
+main = example1
+
+example2 = do
+  putStrLn "start of function"
+  gen <- getGenerator getter parser
+  let go = do
+        next <- gen
+        maybe
+          (putStrLn "end of stream")
+          ( \val -> do
+              action val
+              go
+          )
+          next
+  go
+  putStrLn "end of function"
+
+example1 =
+  let go bs = do
+        next <- getNext getter parser bs
+        maybe
+          (return ())
+          (\(bs', val) -> action val >> go bs')
+          next
+   in go ByteString.empty
+
+getterNoisy = do
+  putStrLn "enter getter for 64K"
+  bs <- ByteString.hGetSome stdin _64K
+  putStrLn $ "exit getter with " ++ show (ByteString.length bs) ++ " bytes"
+  return bs
+
+_64K = 65536 :: Int
+
+_1M = 1024 * 1024 :: Int
+
+_16M = 16 * _1M
+
+getterQuiet = ByteString.hGetSome stdin _16M
+
+getter = getterQuiet
+
+-- type Val = BGPMessage
+-- action = display
+-- parser = bgpParser1
+
+type Val = [BGPMessage]
+
+action :: Val -> IO ()
+action = mapM_ print
+
+parser = bgpParser
+
+display m = putChar $ case m of
+  BGPUpdate {..} ->
+    if  | ByteString.null attributes -> 'E'
+        | null nlri -> 'A'
+        | null withdrawn -> 'u'
+        | otherwise -> 'U'
+  BGPNotify {} -> 'N'
+  BGPKeepalive -> 'K'
+  BGPTimeout -> 'T'
+  BGPEndOfStream -> 'X'
+  (BGPError _) -> '?'
