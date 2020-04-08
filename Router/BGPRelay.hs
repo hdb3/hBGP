d9c147139241cc586f5990a46e99694613f109a7
Nicholas Hart <nic@hdb3.net>
2020-04-08 18:47:30 +0100

    make BGPRelay socket exception safe

--- Router/BGPRelay.hs
+++ Router/BGPRelay.hs
@@ -1,15 +1,15 @@
 module Main where
 
+import Control.Concurrent
+import qualified Data.ByteString as B
 import Data.IP
 import Data.Word
 import Router.BGPConnect
 import Router.BGPPingParser
 import System.Environment (getArgs)
 import System.Exit (die)
-import System.IO(Handle, IOMode(ReadWriteMode))
+import System.IO (Handle, IOMode (ReadWriteMode), hClose)
 import Text.Read (readMaybe)
-import qualified Data.ByteString as B
-import Control.Concurrent
 
 main :: IO ()
 main = do
@@ -35,21 +35,31 @@ listener port local = do
   getSockAddresses sock2 >>= print
   h1 <- socketToHandle sock1 ReadWriteMode
   h2 <- socketToHandle sock2 ReadWriteMode
-  mv1 <- newEmptyMVar
-  mv2 <- newEmptyMVar  
-  thread1 <- forkIO $ tcpForwarder mv1 h1 h2
-  thread2 <- forkIO $ tcpForwarder mv2 h2 h1
-  takeMVar mv1
-  takeMVar mv2
-  gracefulClose sock1 100000
-  gracefulClose sock2 100000
+  c1 <- wrapper $ tcpForwarder h1 h2 "stream 1"
+  c2 <- wrapper $ tcpForwarder h2 h1 "stream 2"
+  r1 <- c1
+  r2 <- c2
+  putStrLn $ "stream1 yielded " ++ show r1
+  putStrLn $ "stream2 yielded " ++ show r2
+  hClose h1
+  hClose h2
   putStrLn "disconnected"
 
-tcpForwarder :: MVar () -> Handle -> Handle -> IO()
-tcpForwarder mvar hIn hOut = do
-  bs <- B.hGetSome hIn 64000000
-  if B.null bs then do
-                       putMVar mvar ()
-                       return ()
-               else do B.hPut hOut bs
-                       tcpForwarder mvar hIn hOut 
+tcpForwarder :: Handle -> Handle -> String -> IO ()
+tcpForwarder hIn hOut name = do
+  putStrLn $ "start " ++ name
+  go
+  putStrLn $ "normal end of stream " ++ name
+  where
+    go = do
+      bs <- B.hGetSome hIn 64000000
+      if B.null bs
+        then return ()
+        else do
+          B.hPut hOut bs
+          go
+
+wrapper action = do
+  mv <- newEmptyMVar
+  forkFinally action (putMVar mv)
+  return (takeMVar mv)
