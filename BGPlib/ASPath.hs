52d062d75ed3c73687eca926d8b96b2f4d92852e
nic hart <nphart@otolonetworks.com>
2021-07-26 08:16:02 +0100

    fix unsignalled overflow error in ASPath parser

--- BGPlib/ASPath.hs
+++ BGPlib/ASPath.hs
@@ -56,7 +56,7 @@ parseASPath n
   | otherwise = do
     t <- A.anyWord8
     l <- A.anyWord8
-    let calculatedByteCount = fromIntegral $ 2 + 4 * l
+    let calculatedByteCount = 2 + 4 * ( fromIntegral l)
     when (n < calculatedByteCount) (error $ "parseASPath: invalid length n=" ++ show n ++ " l=" ++ show l ++ " t=" ++ show t)
     segment <-
       if  | t == enumASSequence -> ASSequence <$> A.count (fromIntegral l) A.anyWord32be
